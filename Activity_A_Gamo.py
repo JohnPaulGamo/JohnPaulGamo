a09b5c91bddd84e2d71554310cf70c979562301c
JohnPaulGamo <45374636+JohnPaulGamo@users.noreply.github.com>
2018-11-26 19:32:17 -0800

    Create Activity_A_Gamo.py

---
+++ Activity_A_Gamo.py
@@ -0,0 +1,12 @@
+
+for x in range(1,20,3):
+     print (x)
+
+for z in range(100,75,-7):
+     print (z)
+
+for x in range(1,5):
+         if x % 4 == 3:
+            print("1" , end = "\t")
+         else:
+             print("0", end="\t")
