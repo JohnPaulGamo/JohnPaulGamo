9bef0eabdb580a8de981234ce18386470fb40271
JohnPaulGamo <45374636+JohnPaulGamo@users.noreply.github.com>
2018-11-26 17:57:38 -0800

    Create Lists_Gamo.py

---
+++ Lists_Gamo.py
@@ -0,0 +1,14 @@
+
+#Lists
+Name_Of_My_Aports = ["Jayson","DJ","Vincent","KoLin","Angelo"]
+Mga_Edad_Nila = [18,18,20,19,18]
+Mga_Puntos_Nila = [0.1,0.2,0.3,0.4]
+
+for x in range (len(Name_Of_My_Aports)):
+    print (Name_Of_My_Aports[x])
+
+for y in range (len(Mga_Edad_Nila)):
+    print (Mga_Edad_Nila[y])
+
+for z in range (len(Mga_Puntos_Nila)):
+    print (Mga_Puntos_Nila[z])
