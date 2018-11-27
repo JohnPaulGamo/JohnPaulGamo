744e09bd96a22c4dd06941654e25f69aa55e330b
JohnPaulGamo <45374636+JohnPaulGamo@users.noreply.github.com>
2018-11-26 19:33:04 -0800

    Create Activity_B_Gamo.py

---
+++ Activity_B_Gamo.py
@@ -0,0 +1,19 @@
+print (1)
+for x in range(8):
+     print(2**x , end = "\t")
+print()
+print(2)
+for y in range(1,13):
+    if y % 4 == 0:
+        print('S', end="\t")
+    else:
+        print(y, end="\t")
+print()
+print ()
+print (3)
+for x in range(1,9):
+     if x  % 8 == 1:
+        print(x, end="\t")
+     else:
+        print ("E", end = "\t")
+print ()
