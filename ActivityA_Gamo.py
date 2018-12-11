9a3f28c5a3dfa37f74f125033461e6876b6296c3
JohnPaulGamo <45374636+JohnPaulGamo@users.noreply.github.com>
2018-12-11 08:57:40 +0800

    Create ActivityA_Gamo.py

---
+++ ActivityA_Gamo.py
@@ -0,0 +1,32 @@
+#Variables
+Friends = 6
+Classmates = 12
+Name = "Poy"
+
+print (Friends)
+print (Classmates)
+print (Name)
+
+#Operators
+Num1 = 20
+Num2 = 10
+
+Num = (Num1 + Num2)
+print(Num)
+NumB = (Num1 - Num2)
+print(NumB)
+
+#Conditionals
+if Num < NumB:
+	print ("Yeko")
+elif Num > NumB:
+	print ("Ednih")
+else:
+	print ("Finish_Na")
+
+#Loops
+for x in range (2,10,3):
+	print ("60puuu")
+
+for x in range (1,11,4):
+	print ("44Hooo")
